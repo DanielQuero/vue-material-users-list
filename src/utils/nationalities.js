a72c97e7decf9f3ffb5e44fc9fdc7ee8d7e19ac2
Daniel Quero <danielquero95@gmail.com>
2021-04-27 22:33:04 +0200

    Added global styles and utils

---
+++ src/utils/nationalities.js
@@ -0,0 +1,21 @@
+'use strict'
+
+export default [
+  { value: 'AU', label: 'Australian' },
+  { value: 'BR', label: 'Brazilian' },
+  { value: 'CA', label: 'Canadian' },
+  { value: 'CH', label: 'Swiss' },
+  { value: 'DE', label: 'German' },
+  { value: 'DK', label: 'Danish' },
+  { value: 'ES', label: 'Spanish' },
+  { value: 'FI', label: 'Finnish' },
+  { value: 'FR', label: 'French' },
+  { value: 'GB', label: 'British' },
+  { value: 'IE', label: 'Irish' },
+  { value: 'IR', label: 'Iranian' },
+  { value: 'NO', label: 'Norwegian' },
+  { value: 'NL', label: 'Dutch' },
+  { value: 'NZ', label: 'New Zealander' },
+  { value: 'TR', label: 'Turkish' },
+  { value: 'US', label: 'American' }
+]
